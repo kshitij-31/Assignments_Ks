505daa9884e73d447f4086ede04084973215cca9
Kshitij Singh <109233109+kshitij-31@users.noreply.github.com>
2022-07-21 12:44:51 +0530

    Add files via upload

---
+++ Calculator1.java
@@ -0,0 +1,24 @@
+package Assignmnet;
+
+public class Calculator1{
+    	public double add(double x, double y) {
+    		double z;
+			z=x+y;
+			return z;
+	}
+    	public double sub(double x, double y) {
+    		double z;
+    		z=x-y;
+    		return z;
+	}
+    	public double mult(double x, double y) {
+    		double z;
+    		z=x*y;
+    		return z;
+	}
+    	public double div(double x, double y) {
+    		double z;
+    		z=x/y;
+    		return z;
+	}
+}
