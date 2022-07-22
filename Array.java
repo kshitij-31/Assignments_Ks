3d232eebfc55eb4278d5e7b06ae31183aef6265a
Kshitij Singh <109233109+kshitij-31@users.noreply.github.com>
2022-07-22 08:41:45 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,16 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]= {2,34,45,2,43};
+		
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+		
+	
+	}
+}
+
+
---
+++ multiDimensionalarray.java
@@ -0,0 +1,22 @@
+
+public class multiDimensionalarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("\n Multidimensional array:\n ");
+
+		
+	int a[][]={{45,30,3},{26,54,45},{4,84,95}};
+
+	//printing 2D array
+	for(int i=0;i<3;i++){
+	 for(int j=0;j<3;j++){
+	   System.out.print(a[i][j]+" ");
+	 }
+	 System.out.println();
+		}
+
+
+	}
+
+}
